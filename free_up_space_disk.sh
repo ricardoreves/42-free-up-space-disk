2a3cf9dc46d8d627aed0f3b83c166dd3f567ea04
ricardoreves <ricardo.reves@gmail.com>
2022-01-23 19:12:47 +0100

    create free_up_space_disk.sh

---
+++ free_up_space_disk.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rm -rf ~/Library/caches/firefox/* -y
+rm -rf ~/Library/caches/vscode-cpptools/* -y
+rm -rf ~/Library/caches/com.tinyspeck.slackmacgap/* -y
+rm -rf ~/Library/caches/com.google.SoftwareUpdate/* -y
+rm -rf ~/Library/caches/com.apple.Safari/* -y
+rm -rf ~/Library/caches/Google/* -y
+rm -rf ~/Library/caches/com.apple.Safari.SafeBrowsing/* -y
+rm -rf ~/Library/caches/com.hnc.Discord/* -y
