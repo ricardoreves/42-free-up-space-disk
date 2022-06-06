7850d00ecedf17b284414823203cdec45966e78a
Ricardo Reves <89393929+ricardoreves@users.noreply.github.com>
2022-06-06 23:49:27 +0200

    Update free_up_space_disk.sh

--- free_up_space_disk.sh
+++ free_up_space_disk.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-rm -rf ~/Library/caches/firefox/* -y
-rm -rf ~/Library/caches/vscode-cpptools/* -y
-rm -rf ~/Library/caches/com.tinyspeck.slackmacgap/* -y
-rm -rf ~/Library/caches/com.google.SoftwareUpdate/* -y
-rm -rf ~/Library/caches/com.apple.Safari/* -y
-rm -rf ~/Library/caches/Google/* -y
-rm -rf ~/Library/caches/com.apple.Safari.SafeBrowsing/* -y
-rm -rf ~/Library/caches/com.hnc.Discord/* -y
+chmod -R 777 ~/Library/Caches/Homebrew &>/dev/null
+rm -rf ~/Library/Caches/* &>/dev/null
+rm -rf ~/Library/Application\ Support/Caches/* &>/dev/null
+rm -rf ~/.Trash/* &>/dev/null
